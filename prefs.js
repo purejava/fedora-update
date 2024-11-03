2af787f20e0a08b1cfa224c1c550017c6e2d1cd4
Ralph Plawetzki <ralph@purejava.org>
2024-11-03 07:17:03 +0100

    Use nicer Adw.EntryRow in perferences window

--- prefs.js
+++ prefs.js
@@ -34,8 +34,6 @@ export default class FedoraUpdatePreferences extends ExtensionPreferences {
 		buildable.get_object('about_name').set_text(this.metadata.name.toString());
 		buildable.get_object('about_version').set_text(this.metadata.version.toString());
 		buildable.get_object('about_description').set_text(this.metadata.description.toString());
-		buildable.get_object('for_dnf4').set_text('dnf 4');
-		buildable.get_object('for_dnf5').set_text('dnf 5');
 		buildable.get_object('about_url').set_markup("<a href=\"" + this.metadata.url.toString() + "\">" + this.metadata.url.toString() + "</a>");
 
 		// Bind fields to settings
